98a8d64c006d09f3e059c3cd57cb7af0e6f25552
Natalie Rayce <natalie.rayce@gmail.com>
2025-06-05 14:34:54 -0400

    Questions

--- Almona/Real EDA.R
+++ Almona/Real EDA.R
@@ -1,4 +1,7 @@
 library(tidyverse)
 nhl_shots <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data/nhl_shots.csv")
 
-#nothing
+#Potential Questions:
+#Home Team Goals vs Away Team Goals
+#Goals by location/position
+
