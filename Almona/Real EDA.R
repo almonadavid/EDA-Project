45330a7d67672c4aca1be5abc6315edb4cd5e15f
David Almona <almonadavid@gmail.com>
2025-06-05 14:38:46 -0400

    Data

--- Almona/Real EDA.R
+++ Almona/Real EDA.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 nhl_shots <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data/nhl_shots.csv")
 
-#Potential Questions:
-#Home Team Goals vs Away Team Goals
-#Goals by location/position
+# Potential Questions:
+# Home Team Goals vs Away Team Goals
+# Shot by location/position
+# Shot angle
 
