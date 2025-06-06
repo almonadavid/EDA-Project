edce0c08af659dd5abfbc28ad69c36fbdfda4f16
Oliver Daboo <oliverdaboo@icloud.com>
2025-06-06 13:31:00 -0400

    Started analysis

--- Daboo/EDA_Daboo.R
+++ Daboo/EDA_Daboo.R
@@ -1,4 +1,18 @@
+
+# loading the data
 library(tidyverse)
 nhl_shots <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data/nhl_shots.csv")
 
 
+nhl_goals<- nhl_shots|>
+  filter(event %in% "GOAL" )
+
+library(ggplot2)
+
+nhl_goals|>
+  count(event, period)
+
+nhl_goals|>
+  select(event, period)|>
+  ggplot(aes(period))+
+  geom_bar()
