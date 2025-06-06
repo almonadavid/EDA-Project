fb48d5ee5a9dcaadcee23c535373f969825722d4
Oliver Daboo <oliverdaboo@icloud.com>
2025-06-06 14:47:18 -0400

    Looking at how many goals each team scores per period

--- Daboo/EDA_Daboo.R
+++ Daboo/EDA_Daboo.R
@@ -12,7 +12,51 @@ library(ggplot2)
 nhl_goals|>
   count(event, period)
 
+# visually looking at the goals scored in each period with a bar chart
 nhl_goals|>
   select(event, period)|>
   ggplot(aes(period))+
   geom_bar()
+
+# creating a data set with just the goals scored in regulation
+nhl_goals_reg <- nhl_goals |>
+  filter(period %in% c(1, 2, 3))
+
+# bar chart with counts and proportions
+nhl_goals_reg|>
+  select(event, period)|>
+  count(period)|>
+  mutate(prop=n/sum(n))|>
+  ggplot(aes(period, prop))+
+  geom_col()+
+  geom_label(aes(label=n))
+
+# bar chart showing how many goals each teams scores in each period
+nhl_goals_reg|>
+  select(event, period, teamCode)|>
+  ggplot(aes(period, color=teamCode))+
+  geom_bar()
+
+# table showing how many goals each team scores in each period
+nhl_goals_reg|>
+  select(event, period, teamCode)|>
+  count(period, teamCode) |>
+  pivot_wider(names_from=teamCode, values_from=n)
+
+# table showing the proportions of goals in each period that each team scores 
+nhl_goals_reg_period_team<- nhl_goals_reg|>
+  select(event, period, teamCode)|>
+  count(period, teamCode) |>
+  group_by(period)|>
+  mutate(prop=n/sum(n))|>
+  select(period, teamCode, prop)|>
+  pivot_wider(names_from=teamCode, values_from=prop)|>
+  ungroup()|>
+  mutate(total=rowSums(across(-period)))
+
+# Looking at who were the best shooters by SOG%
+
+nhl_shots_reg<- nhl_shots|>
+  filter(period %in% c(1, 2, 3))
+
+class(nhl_shots_reg$event)
