3ef6be3242dd913191a4a1dbd9dce766e1b0b3f2
David Almona <almonadavid@gmail.com>
2025-06-14 17:42:39 -0400

    NHL

--- Almona/Real EDA.R
+++ Almona/Real EDA.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(sportyR)
 library(ggplot2)
 library(ggrepel)
+library(factoextra)
 theme_set(theme_light())
 nhl_shots <- read_csv("https://raw.githubusercontent.com/36-SURE/2025/main/data/nhl_shots.csv")
 
